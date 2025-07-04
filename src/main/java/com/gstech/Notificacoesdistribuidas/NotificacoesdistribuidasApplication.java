cda86149d1cf4d74ad2b5c32b5dbd8476071acaf
Guilherme-Saar <guilhermesaar02@gmail.com>
2025-07-04 19:40:58 -0300

    initial commit

---
+++ src/main/java/com/gstech/Notificacoesdistribuidas/NotificacoesdistribuidasApplication.java
@@ -0,0 +1,13 @@
+package com.gstech.Notificacoesdistribuidas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificacoesdistribuidasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificacoesdistribuidasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gstech/Notificacoesdistribuidas/NotificacoesdistribuidasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gstech.Notificacoesdistribuidas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificacoesdistribuidasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
